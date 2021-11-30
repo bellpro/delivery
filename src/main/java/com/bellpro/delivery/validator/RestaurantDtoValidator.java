53f0146c2e18d76446187005ceffb1dec1e15a3f
bellpro <kju1909020@gmail.com>
2021-11-30 21:28:00 +0900

    음식점 등록 및 조회 테스트 코드 완료

--- src/main/java/com/bellpro/delivery/validator/RestaurantDtoValidator.java
+++ src/main/java/com/bellpro/delivery/validator/RestaurantDtoValidator.java
@@ -13,7 +13,7 @@ public class RestaurantDtoValidator {
         } else if (restaurantDto.getMinOrderPrice() % 100 != 0){
             throw new IllegalArgumentException("최소주문 가격은 100원 단위로만 입력 가능합니다.");
         }
-        if (restaurantDto.getDeliveryFee() > 10000 ){
+        if (restaurantDto.getDeliveryFee() < 0 || restaurantDto.getDeliveryFee() > 10000 ){
             throw new IllegalArgumentException("배달비는 0원 ~ 10,000원을 입력해주세요.");
         } else if (restaurantDto.getDeliveryFee() % 500 != 0){
             throw new IllegalArgumentException("배달비는 500원 단위로만 입력 가능합니다.");
